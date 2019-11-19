22afd51741bad0a1f78e8ce6edf7a61ab685a930
Will Powell <William.Powell@softwire.com>
2019-11-19 17:04:54 +0000

    Streams

--- src/com/company/FizzBuzzBangStream.java
+++ src/com/company/FizzBuzzBangStream.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class FizzBuzzBangStream {
     public static Stream<String> create() {
+        String[] arr = new String[100];
+        for (int i = 0; i < 100; ++i) {
+            arr[i] = i;
+        }
+        return Arrays.stream(arr);
+    }
 
+    public static void main(String[] args) {
+        FizzBuzzBangStream.create().forEach(System.out::println);
     }
 }
