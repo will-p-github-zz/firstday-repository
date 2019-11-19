f06357c52c261ee93d2f3a92d92904926c512c28
Will Powell <William.Powell@softwire.com>
2019-11-19 14:29:52 +0000

    FizzBuzz!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    for (int i = 1; i <= 100; i++) {
+	        if (i % 3 == 0 && i % 5 == 0) {
+	            System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+	            System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
